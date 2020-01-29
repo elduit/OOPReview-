beace28a56e1e00d118cd25ae2c8e977ccb8ab92
Eduardo Fernández <eduardofrndzrdrgz@gmail.com>
2020-01-29 13:22:23 +0100

    Update

--- Library.ts
+++ Library.ts
@@ -4,7 +4,7 @@ export class mobileLibrary {
     private location : string;
     private moviles : mobile[]
     private totalprice : number;
-    constructor(name : string, location : string, moviles: mobile[]){
+    constructor(name : string, location : string, moviles: mobile[], totalprice : number){
         name = this.name;
         location = this.location;
         moviles = this.moviles;
@@ -13,7 +13,7 @@ export class mobileLibrary {
     getName(){return this.name};
     getLocation(){return this.location};
     getMoviles(){return this.moviles};
-    getTotalPrice(){return this.totalprice};
+    getTotalPrice(){return this.totalPriceCalc()};
     // setters
     setName(entrada):string{
         this.name = entrada;
@@ -24,15 +24,19 @@ export class mobileLibrary {
     };setmoviles(entrada):mobile[]{
         this.moviles = entrada;
         return this.moviles;
-    };settotalprice(entrada):number{
-        this.totalprice = entrada;
-        return this.totalprice;
     }
-    totalPriceCalc(){
+    private totalPriceCalc(){
         let resultado : number = 0;
         for (let i = 0; i < this.moviles.length; i++) {
             resultado = resultado + this.moviles[i].getPrice();
         }
         console.log(resultado);
+    }
+    pritAll(){
+        console.log("This is all my mobiles: \n\t");
+        for (let i = 0; i < this.moviles.length; i++) {
+            this.moviles[i].printAll()
+        }
+        console.log("Price overall" + this.getTotalPrice());
     }
 }
--- Main1.ts
+++ Main1.ts
@@ -34,7 +34,48 @@ movil2.setIs5G(true);
 movil2.setCameraNumber(3);
 movil2.setPrice(350);
 // Library 
-let library0 = new mobileLibrary("","",[])
+let library0 = new mobileLibrary("","",[],0)
 library0.setName("Biblioteca moviles");
 library0.setLocation("Madrid");
 library0.setmoviles([movil0,movil1,movil2]);
+library0.pritAll();
+
+// Nokia3210
+let movil01 = new mobile("","","",0,"",false,0,0);
+movil01.setName("Nokia3210");
+movil01.setModel("3210");
+movil01.settrademark("Nokia");
+movil01.setSdSize(16);
+movil01.setColor("gris");
+movil01.setIs5G(false);
+movil01.setCameraNumber(0);
+movil01.setPrice(150);
+// Modificar Nokia
+movil01.setIs5G(true);
+movil01.setCameraNumber(5);
+// Iphone3G
+let movil11 = new mobile("","","",0,"",false,0,0);
+movil11.setName("Iphone3G");
+movil11.setModel("3G");
+movil11.settrademark("Apple");
+movil11.setSdSize(32);
+movil11.setColor("blanco");
+movil11.setIs5G(false);
+movil11.setCameraNumber(3);
+movil11.setPrice(290);
+// Galaxy10
+let movil21 = new mobile("","","",0,"",false,0,0);
+movil21.setName("Samsung Galaxy 10");
+movil21.setModel("Galaxy 10");
+movil21.settrademark("Samsung");
+movil21.setSdSize(64);
+movil21.setColor("negro");
+movil21.setIs5G(true);
+movil21.setCameraNumber(3);
+movil21.setPrice(350);
+// Library 
+let library01 = new mobileLibrary("","",[],0)
+library01.setName("Biblioteca moviles");
+library01.setLocation("Madrid");
+library01.setmoviles([movil0,movil1,movil2]);
+library01.pritAll();
